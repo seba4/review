4f7cd8c942a999abe71a96f2cfbc638432ee9440
anzyt <at4731@student.uni-lj.si>
2020-12-18 15:17:41 +0100

    Added credentials for firebase production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyDNMwea8VQdUM4UU794fEm8sUnSxZ6QYNI",
+    authDomain: "review-42f31.firebaseapp.com",
+    databaseURL: "https://review-42f31.firebaseio.com",
+    projectId: "review-42f31",
+    storageBucket: "review-42f31.appspot.com",
+    messagingSenderId: "1022606889271",
+    measurementId: "G-J4YJ75ZQBL",
+    appId: "1:1022606889271:web:79f8114ea5c0078d224ab2",
+  }
 };
